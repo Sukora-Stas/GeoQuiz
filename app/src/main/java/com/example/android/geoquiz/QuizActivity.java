577d64b48936a14ae40edd6f6aca3961cc2ffde7
Sukora Stas <stasiksukora@mail.ru>
2017-10-20 21:29:17 +0300

    GeoQuiz: add Toast

--- app/src/main/java/com/example/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/android/geoquiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -17,13 +18,25 @@ public class QuizActivity extends AppCompatActivity {
 
         mTrueButton = (Button) findViewById(R.id.true_button);
 
-        mTrueButton.setOnClickListener(new View.OnClickListener(){
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
             }
         });
 
         mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
