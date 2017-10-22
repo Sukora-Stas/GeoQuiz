634f098c5f03508210cc32c65fe4fb5493089384
Sukora Stas <stasiksukora@mail.ru>
2017-10-22 11:50:08 +0300

    GeoQuiz

--- app/src/main/java/com/example/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/android/geoquiz/QuizActivity.java
@@ -71,6 +71,15 @@ public class QuizActivity extends AppCompatActivity {
 
 
     private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
