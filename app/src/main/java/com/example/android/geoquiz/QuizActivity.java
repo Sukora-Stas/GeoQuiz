e5e53403f760e78cc1f0d2548713a20f5e58348f
Sukora Stas <stasiksukora@mail.ru>
2017-10-25 21:40:58 +0300

    GeoQuiz

--- app/src/main/java/com/example/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/android/geoquiz/QuizActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -29,6 +30,18 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_americas, true),
             new Question(R.string.question_asia, true)
     };
+
+
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,7 +93,13 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
         updateQuestion();
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
     }
+
 
     @Override
     protected void onStart() {
